98c838a47fcb02e544a65b3af3088ace1a229889
Nirasaka <ipheion0903@gmail.com>
2026-07-13 10:18:53 +0900

    summonMonster

--- app/src/main/java/org/example/Monster.java
+++ app/src/main/java/org/example/Monster.java
@@ -1,10 +1,15 @@
 package org.example;
 
 public class Monster {
-    private String name = "デュラハン";
-    private int rare = 0;
+  public String name;
+  public int rare;
 
-    public String toString() {
-        return name + ":レア度[" + rare + "]";
-    }
+  public String toString() {
+    return name + ":レア度[" + rare + "]";
+  }
+
+  public String summonMonster(int rare) {
+    String monster[] = { "デュラハン", "スライム", "サハギン", "ドラゴン", "リヴァイアサン" };
+    return monster[rare];
+  }
 }
--- app/src/test/java/org/example/MonsterTest.java
+++ app/src/test/java/org/example/MonsterTest.java
@@ -4,8 +4,20 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MonsterTest {
-    @Test public void toStringTest(){
-        Monster monster = new Monster();
-        assertNotNull("デュラハン:レア度[0]",monster.toString());
-    }
+  @Test
+  public void toStringTest() {
+    Monster monster = new Monster();
+    monster.name = "デュラハン";
+    monster.rare = 0;
+
+    assertNotNull("デュラハン:レア度[0]", monster.toString());
+  }
+
+  @Test
+  public void summonMonsterTest() {
+    Monster monster = new Monster();
+    int rare = 1;
+    String m = "スライム";
+    assertNotNull(m, monster.summonMonster(rare));
+  }
 }
